0529b833cf8afeb847dc97e2f70714df80fab7e8
sourav <souravsengupta20192019@gmail.com>
2022-01-21 00:09:58 +0530

    Latest Code 2

--- src/main/java/controller/ItemController.java
+++ src/main/java/controller/ItemController.java
@@ -5,10 +5,16 @@ public class ItemController {
     private final String mname="petter";
     private final String lname="world";
     private final String father_name="world123";
-    public static void det_2(){
+    public static void func_1){
         System.out.println("Hello");
     }
-    public static void det_1(){
+    public static void func_2(){
+        System.out.println("Hello");
+    }
+    public static void func_3(){
+        System.out.println("Hello");
+    }
+    public static void func_4(){
         System.out.println("Hello");
     }
 }
