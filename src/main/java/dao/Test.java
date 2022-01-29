af11f3f4a71df1b56c49c58a19cb8318024e1ce5
sourav20212021 <82802288+sourav20212021@users.noreply.github.com>
2022-01-29 13:20:08 +0530

    Update Test.java

--- src/main/java/dao/Test.java
+++ src/main/java/dao/Test.java
@@ -1,12 +1,18 @@
 package dao;
 
 public class Test {
+   void case_1(){
+    System.out.println("dev-1");
+  }
   void disp_2(){
     System.out.println("dev-1");
   }
   void disp_3(){
     System.out.println("dev-2");
       System.out.println("dev-3");
+  }
+   void case_2(){
+    System.out.println("dev-1");
   }
     void msg_1(){
         System.out.println("hello-1");
