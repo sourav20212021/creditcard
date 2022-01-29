f0ac70df48b1ede44fb96149faa540f050c9941c
sourav20212021 <82802288+sourav20212021@users.noreply.github.com>
2022-01-29 13:19:20 +0530

    Update Lorem.java

--- src/main/java/dao/Lorem.java
+++ src/main/java/dao/Lorem.java
@@ -3,7 +3,13 @@ package dao;
 public class Lorem {
     static int x=0;
     final String fname="LoremIpsum";
+    void show2(){
+        System.out.println(fname);
+    }
     void disp1(){
+        System.out.println(fname);
+    }
+    void show1(){
         System.out.println(fname);
     }
     void disp2(){
