917b57b418fd24522b8a2832aa309e53efb0eff0
sourav <souravsengupta20192019@gmail.com>
2022-12-07 20:49:50 +0530

    Commit-7

---
+++ src/main/java/controller/Stock.java
@@ -0,0 +1,21 @@
+package controller;
+
+public class ItemController {
+	
+	static String name="Jack";
+	
+	public String getName(){
+		return "HelloJack";
+	}
+	
+	public String getFirstName(){
+		return "HelloJack";
+	}
+	
+	public String getLastName(){
+		return "HelloJonathon";
+	}
+	public String getMiddleName(){
+		return "HelloJonathon";
+	}
+}
