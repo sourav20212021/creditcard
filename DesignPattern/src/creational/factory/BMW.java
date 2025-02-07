d669d84351272d64fcff2979abf5e946985a795a
sourav <souravsengupta20192019@gmail.com>
2025-02-08 01:03:18 +0530

    "fourth_004_rollback"
    This reverts commit d28b8fcccc9c03b4ff62beb0e7f817a7706d32e8.

--- DesignPattern/src/creational/factory/BMW.java
+++ DesignPattern/src/creational/factory/BMW.java
@@ -11,9 +11,5 @@ public class BMW implements Car{
 		
 		System.out.println("This is price");
 	}
-	
-	public void qa_desc_1(){
-		System.out.println("qa_desc_1");
-	}
 
 }
--- DesignPattern/src/creational/factory/Honda.java
+++ DesignPattern/src/creational/factory/Honda.java
@@ -5,8 +5,4 @@ public class Honda implements Car{
 		// TODO Auto-generated method stub
 		System.out.println("This is Honda");
 	}
-	
-	public void qa_desc_2(){
-		System.out.println("qa_desc_2");
-	}
 }
