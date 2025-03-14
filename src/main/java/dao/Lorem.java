c4cfde3836148914f49736aa8522ad25a4135679
sourav <souravsengupta20192019@gmail.com>
2025-03-14 23:27:42 +0530

    Latest File changes-3

--- src/main/java/dao/Lorem.java
+++ src/main/java/dao/Lorem.java
@@ -25,13 +25,7 @@ public class Lorem {
     void disp_x1(){
         System.out.println("Hello-1");
         System.out.println("Hello-2");
+        System.out.println("Hello-3");
     }
 
-    void disp3(){
-    	System.out.println("Lorem Dolar-1");
-    }
-
-    void disp4(){
-    	System.out.println("Lorem Dolar-2");
-    }
 }
