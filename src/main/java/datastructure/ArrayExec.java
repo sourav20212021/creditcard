153e2cd1b22c7b7486dfdd219268e14d6f7ea419
sourav <souravsengupta20192019@gmail.com>
2025-03-14 19:03:45 +0530

    Latest File changes-1

---
+++ src/main/java/datastructure/ArrayExec.java
@@ -0,0 +1,10 @@
+package datastructure;
+
+public class ArrayExec {
+    public static void main(String[] args)
+    {
+        System.out.println("I am a Geek");
+        System.out.println("I am a Geek2");
+        System.out.println("I am a Geek3");
+    }
+}
