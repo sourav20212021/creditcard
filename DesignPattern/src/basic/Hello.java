93f996c05e9692b8842e40c45b6011fe9ee30ccb
sourav <souravsengupta20192019@gmail.com>
2025-03-14 23:34:55 +0530

    comm-1

--- DesignPattern/src/basic/Hello.java
+++ DesignPattern/src/basic/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 		System.out.println("Hello World");
 		System.out.println("Hello World1");
 		System.out.println("Hello World2");
+		System.out.println("Hello World3");
 	}
 
 }
